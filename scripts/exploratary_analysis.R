2ad28866234f4bf5fa0604b13abb8840f55df8ba
SenuriPereraTECHS <damdiniperera01@gmail.com>
2026-03-22 22:05:03 +0530

    installed necessary libraries and packages, view dims, cols, data types and check missing values

--- scripts/exploratary_analysis.R
+++ scripts/exploratary_analysis.R
@@ -1,3 +1,22 @@
+library(tidyverse) # Includes ggplot2 for charts and dplyr for tables
+library(corrplot)  # heatmap
+library(scales) #format chart axes
+
+#install packages
+install.packages(c("tidyverse", "corrplot", "scales"))
+
 #load the preprocessed dataset
 df <- read.csv("data/processed/cleaned_hr_data.csv")
 head(df)
+
+#view dimensions and columns and check data types
+dim(df)
+colnames(df)
+str(df)
+
+#check missing values
+sum(is.na(df))
+
+
+
+
