ab399ceda6ed07c1945cb9e0c10652cb7bb73301
HiruniNv <hiruninavoda1@gmail.com>
2026-04-07 19:25:21 +0530

    Implement Tukey HSD for pairwise group comparisons

--- scripts/03_hypothesis_testing.R
+++ scripts/03_hypothesis_testing.R
@@ -20,3 +20,30 @@ print(correlation_result)
 
 # Save output
 capture.output(correlation_result, file = "results/tables/correlation_results.txt")
+
+# 2. ANOVA Test
+# Testing if mean PerformanceRating differs across WorkLifeBalance groups
+anova_model <- aov(PerformanceRating ~ WorkLifeBalance, data = data)
+anova_summary <- summary(anova_model)
+print(anova_summary)
+
+# Save output
+capture.output(anova_summary, file = "results/tables/anova_results.txt")
+
+# 3. Normality Test (Residuals)
+shapiro_result <- shapiro.test(residuals(anova_model))
+print(shapiro_result)
+capture.output(shapiro_result, file = "results/tables/shapiro_test.txt")
+
+# 4. Homogeneity of Variance
+if(!require(car)) install.packages("car") 
+library(car)
+
+# Ensure it is a factor (This fixes the error)
+data$WorkLifeBalance <- as.factor(data$WorkLifeBalance)
+
+levene_result <- leveneTest(PerformanceRating ~ WorkLifeBalance, data = data)
+print(levene_result)
+
+# Save output
+capture.output(levene_result, file = "results/tables/levene_test.txt")
