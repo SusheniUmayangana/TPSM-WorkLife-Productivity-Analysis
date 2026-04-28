0a2c42a4f308d369e62530229b265c02bd3b76f7
HiruniNv <hiruninavoda1@gmail.com>
2026-04-28 11:39:51 +0530

    data: import updated preprocessed dataset for script 03

--- scripts/03_hypothesis_testing.R
+++ scripts/03_hypothesis_testing.R
@@ -3,7 +3,6 @@
 # Member: Navoda D G H (IT23265806)
 # Goal: Perform correlation, hypothesis testing, ANOVA, and post-hoc analysis
 # -------------------------------------------------------------------------
-
 # 1. Load Data and Setup Factors
 data <- read.csv("data/processed/cleaned_hr_data.csv")
 data$WorkLifeBalance <- as.factor(data$WorkLifeBalance)
